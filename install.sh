278a8babe22b0207dff286dc550c3158a309a7bb
Alan Voiski <alannunesv@gmail.com>
2018-06-18 16:51:35 -0700

    No php please
    At least for now.

--- install.sh
+++ install.sh
@@ -18,17 +18,17 @@ brew bundle
 chsh -s $(which zsh)
 
 # Install PHP extensions with PECL
-pecl install imagick
+# pecl install imagick
 
 # Install Composer
-curl -sS https://getcomposer.org/installer | php
-mv composer.phar /usr/local/bin/composer
+# curl -sS https://getcomposer.org/installer | php
+# mv composer.phar /usr/local/bin/composer
 
 # Install global Composer packages
-/usr/local/bin/composer global require laravel/installer laravel/spark-installer laravel/valet
+# /usr/local/bin/composer global require laravel/installer laravel/spark-installer laravel/valet
 
 # Install Laravel Valet
-$HOME/.composer/vendor/bin/valet install
+# $HOME/.composer/vendor/bin/valet install
 
 # Install global NPM packages
 npm install --global yarn
