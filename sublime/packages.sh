f3f9e9d5868ba49061a4690ec4347e57bb557acd
Alan Voiski <alannunesv@gmail.com>
2022-06-16 16:40:15 -0300

    Fix sublime config
    Remove the deprecated material bar and fix path reference for its package management installation.

--- sublime/packages.sh
+++ sublime/packages.sh
@@ -1,11 +1,11 @@
 #!/bin/bash -e
-BASE="${HOME}/Library/Application Support/Sublime Text 3"
+BASE="${HOME}/Library/Application Support/Sublime Text"
 PACKAGES="${BASE}/Installed Packages"
 SETTINGS="${BASE}/Packages/User"
 
-mkdir -p $PACKAGES
-wget https://packagecontrol.io/Package%20Control.sublime-package -P ${PACKAGES}
+mkdir -p "$PACKAGES"
+wget https://packagecontrol.io/Package%20Control.sublime-package -P "${PACKAGES}"
 
-mkdir -p $SETTINGS
-rm -Rf $SETTINGS
-ln -s "${HOME}/.dotfiles/sublime/User" $SETTINGS
+mkdir -p "$SETTINGS"
+rm -Rf "$SETTINGS"
+ln -s "${HOME}/.dotfiles/sublime/User" "$SETTINGS"
